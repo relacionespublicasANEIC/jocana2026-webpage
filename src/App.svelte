214f0abbd58c534db553e38da3cc9cfdc9866281
Jorge David Bacca Agudelo <288199037+jbaccaa@users.noreply.github.com>
2026-06-24 03:38:44 -0500

    Add content and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,43 @@
-<h1 class="text-3xl text-red-300 font-bold underline">Hello world!</h1>
+<section class="space-y-4">
+        <header
+                class="flex flex-col relative justify-center align-middle w-3/4"
+        >
+                <img
+                        class="w-full md:w-1/12 dark:grayscale"
+                        src="./jocana-icon.webp"
+                        alt="Logo principal del Jocana 2026"
+                />
+
+                <h1 class="text-6xl dark:text-white">
+                        Jornada de Capacitación Nacional
+                </h1>
+        </header>
+        <nav class="flex space-x-4">
+                <div
+                        class="p-2 border-1 rounded-sm dark:bg-zinc-600 shadow-md dark:text-white"
+                >
+                        Del 18 al 20 de agosto de 2026
+                </div>
+
+                <div
+                        class="p-2 border-1 rounded-sm dark:bg-zinc-600 shadow-md dark:text-white"
+                >
+                        Evento completamente virtual
+                </div>
+
+                <a
+                        class="p-2 border-1 rounded-sm dark:bg-zinc-600 shadow-md dark:text-white cursor-pointer"
+                        href="https://instagram.com/aneiccolombia"
+                >
+                        Organiza la ANEIC Colombia ↗
+                </a>
+        </nav>
+</section>
+
+<section>
+        <iframe
+                class="w-full h-96"
+                src="https://forms.gle/X64XS5o3pHe9XoMq5"
+                title="Formulario de registro"
+        ></iframe>
+</section>
